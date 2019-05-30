ab5d29b7e2bfc79789a67d04095ba09477e08f59
MihaiZ <zaporojanmihai@gmail.com>
2019-05-30 14:55:48 +0300

    push with cv

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,13 +15,13 @@ window.addEventListener('load', function() {
 //								COUNT UP SCRIPT
 //------------------------------------------------------------------------------------
 
-var benefits_3col_counter_3 = $('#benefits-3col-counter-3').waypoint({
+var benefits_2col_counter_2 = $('#benefits-2col-counter-2').waypoint({
 	handler: function(direction) {
 		$(this.element).find('.count-up-data').each(function(i, el){
 			var endVal = el && Number.isInteger(el.textContent * 1) ? el.textContent * 1 : 100 ;
 			$(el ).countup({endVal: endVal, options: { separator : ''}});
 		});
-        benefits_3col_counter_3[0].disable();
+        benefits_2col_counter_2[0].disable();
 	},
 	offset: '90%'
 });
